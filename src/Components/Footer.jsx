5e71ae87e976df0eb59f8462c83dc551669a70c1
BomsG <bomageorge572@gmail.com>
2024-07-27 11:08:41 +0100

    refactor: fixed build errors

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,124 +1,109 @@
-"use client";
-import React from "react";
-import Image from "next/image";
-import socials from "../../public/images/socials.jpeg";
-import logo from "../../public/images/logo2.png";
-import { FaFacebook, FaWhatsapp, FaInstagram } from "react-icons/fa";
-import { motion } from "framer-motion";
-import Link from "next/link";
+'use client';
+import React from 'react';
+import Image from 'next/image';
+import socials from '../../public/images/socials.jpeg';
+import logo from '../../public/images/logo2.png';
+import { FaFacebook, FaWhatsapp, FaInstagram } from 'react-icons/fa';
+import { motion } from 'framer-motion';
+import Link from 'next/link';
 
 const Footer = () => {
   const li = [
     {
       id: 1,
-      list: "HOME",
-      link: "/",
+      list: 'HOME',
+      link: '/',
     },
     {
       id: 2,
-      list: "ABOUT US",
-      link: "About",
+      list: 'ABOUT US',
+      link: 'About',
     },
     {
       id: 3,
-      list: "SERVICES",
-      link: "/",
+      list: 'SERVICES',
+      link: '/',
     },
     {
       id: 4,
-      list: "GALLERY",
-      link: "/",
+      list: 'GALLERY',
+      link: '/',
     },
     {
       id: 5,
-      list: "CONTACT US",
-      link: "/",
+      list: 'CONTACT US',
+      link: '/',
     },
   ];
   return (
     <>
-      <footer className="bg-white py-10 md:py-16 mt-80">
-        <div className="container max-w-screen-xl mx-auto px-4">
-          <div className="flex flex-col lg:flex-row justify-between">
-            <div className="text-center lg:text-left mb-10 lg:mb-0">
-              <div className="flex justify-center lg:justify-start mb-5">
-                <img
-                  src="images/logo.png"
-                  alt="Logo img"
-                  className="w-32 md:-80 lg:w-ful"
-                />
+      <footer className='bg-gray-100 py-10 md:py-16 mt-80'>
+        <div className='container max-w-screen-xl mx-auto px-4'>
+          <div className='flex flex-col lg:flex-row justify-between'>
+            <div className='text-center lg:text-left mb-10 lg:mb-0'>
+              <div className='flex justify-center lg:justify-start mb-5'>
+                <img src='images/logo.png' alt='Logo img' className='w-32 md:-80 lg:w-ful' />
               </div>
 
-              <p className="font-light text-gray-400 text-xl mb-10">
-                Get your dream house with
-              </p>
+              <p className='font-light text-gray-400 text-xl mb-10'>Get your dream house with</p>
 
-              <div className="flex items-center justify-center lg:justify-start space-x-5">
+              <div className='flex items-center justify-center lg:justify-start space-x-5'>
                 <a
-                  href="#"
-                  className="px-3 py-3 bg-gray-200 text-gray-700 rounded-full hover:bg-[#3D95D1] hover:text-white transition ease-in-out duration-500"
+                  href='#'
+                  className='px-3 py-3 bg-gray-200 text-gray-700 rounded-full hover:bg-[#3D95D1] hover:text-white transition ease-in-out duration-500'
                 >
-                  <i data-feather="facebook"></i>
+                  <i data-feather='facebook'></i>
                 </a>
 
                 <a
-                  href="#"
-                  className="px-3 py-3 bg-gray-200 text-gray-700 rounded-full hover:bg-[#3D95D1] hover:text-white transition ease-in-out duration-500"
+                  href='#'
+                  className='px-3 py-3 bg-gray-200 text-gray-700 rounded-full hover:bg-[#3D95D1] hover:text-white transition ease-in-out duration-500'
                 >
-                  <i data-feather="twitter"></i>
+                  <i data-feather='twitter'></i>
                 </a>
 
                 <a
-                  href="#"
-                  className="px-3 py-3 bg-gray-200 text-gray-700 rounded-full hover:bg-[#3D95D1] hover:text-white transition ease-in-out duration-500"
+                  href='#'
+                  className='px-3 py-3 bg-gray-200 text-gray-700 rounded-full hover:bg-[#3D95D1] hover:text-white transition ease-in-out duration-500'
                 >
-                  <i data-feather="linkedin"></i>
+                  <i data-feather='linkedin'></i>
                 </a>
               </div>
             </div>
 
-            <div className="text-center lg:text-left mb-10 lg:mb-0">
-              <h4 className="font-semibold text-gray-900 text-2xl mb-6">
-                Sitemap
-              </h4>
+            <div className='text-center lg:text-left mb-10 lg:mb-0'>
+              <h4 className='font-semibold text-gray-900 text-2xl mb-6'>Useful links</h4>
 
               <a
-                href="#"
-                className="block font-light text-gray-400 text-xl mb-6 hover:text-gray-800 transition ease-in-out duration-300"
+                href='#'
+                className='block font-light text-gray-400 text-xl mb-6 hover:text-gray-800 transition ease-in-out duration-300'
               >
                 Home
               </a>
 
               <a
-                href="#"
-                className="block font-light text-gray-400 text-xl mb-6 hover:text-gray-800 transition ease-in-out duration-300"
+                href='/services'
+                className='block font-light text-gray-400 text-xl mb-6 hover:text-gray-800 transition ease-in-out duration-300'
               >
                 Features
               </a>
 
               <a
-                href="#"
-                className="block font-light text-gray-400 text-xl mb-6 hover:text-gray-800 transition ease-in-out duration-300"
+                href='#'
+                className='block font-light text-gray-400 text-xl mb-6 hover:text-gray-800 transition ease-in-out duration-300'
               >
                 Gallery
               </a>
 
               <a
-                href="#"
-                className="block font-light text-gray-400 text-xl mb-6 hover:text-gray-800 transition ease-in-out duration-300"
-              >
-                Testimoni
-              </a>
-
-              <a
-                href="#"
-                className="block font-light text-gray-400 text-xl mb-6 hover:text-gray-800 transition ease-in-out duration-300"
+                href='/contact'
+                className='block font-light text-gray-400 text-xl mb-6 hover:text-gray-800 transition ease-in-out duration-300'
               >
                 Book a meeting
               </a>
             </div>
 
-            <div className="text-center lg:text-left mb-10 lg:mb-0">
+            {/* <div className="text-center lg:text-left mb-10 lg:mb-0">
               <h4 className="font-semibold text-gray-900 text-2xl mb-6">
                 Landing
               </h4>
@@ -164,23 +149,21 @@ const Footer = () => {
               >
                 Donation
               </a>
-            </div>
+            </div> */}
 
-            <div className="text-center lg:text-left">
-              <h4 className="font-semibold text-gray-900 text-2xl mb-6">
-                Utility
-              </h4>
+            <div className='text-center lg:text-left'>
+              <h4 className='font-semibold text-gray-900 text-2xl mb-6'>Utility</h4>
 
               <a
-                href="#"
-                className="block font-light text-gray-400 text-xl mb-6 hover:text-gray-800 transition ease-in-out duration-300"
+                href='#'
+                className='block font-light text-gray-400 text-xl mb-6 hover:text-gray-800 transition ease-in-out duration-300'
               >
                 FAQ
               </a>
 
               <a
-                href="#"
-                className="block font-light text-gray-400 text-xl mb-6 hover:text-gray-800 transition ease-in-out duration-300"
+                href='#'
+                className='block font-light text-gray-400 text-xl mb-6 hover:text-gray-800 transition ease-in-out duration-300'
               >
                 Terms & Conditions
               </a>
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,13 +1,14 @@
-"use client";
+'use client';
 
 /* eslint-disable react-hooks/rules-of-hooks */
-import React, { useState } from "react";
-import Image from "next/image";
-import logo from "../../public/images/logo.png";
-import { motion } from "framer-motion";
-import Link from "next/link";
-import { CiMenuFries } from "react-icons/ci";
-import { MdOutlineRestaurantMenu } from "react-icons/md";
+import React, { useState } from 'react';
+import Image from 'next/image';
+import logo from '../../public/images/logo.png';
+import { motion } from 'framer-motion';
+import Link from 'next/link';
+import { CiMenuFries } from 'react-icons/ci';
+import { MdOutlineRestaurantMenu } from 'react-icons/md';
+import { IoCloseOutline } from 'react-icons/io5';
 
 const Nav = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -18,79 +19,73 @@ const Nav = () => {
   const li = [
     {
       id: 1,
-      list: "HOME",
-      link: "/",
+      list: 'HOME',
+      link: '/',
     },
     {
       id: 2,
-      list: "ABOUT US",
-      link: "About",
+      list: 'ABOUT US',
+      link: '/about',
     },
     {
       id: 3,
-      list: "SERVICES",
-      link: "/services",
+      list: 'SERVICES',
+      link: '/services',
     },
     {
       id: 4,
-      list: "GALLERY",
-      link: "/Gallery",
+      list: 'GALLERY',
+      link: '/gallery',
     },
     {
       id: 5,
-      list: "CONTACT US",
-      link: "/Contact",
+      list: 'CONTACT US',
+      link: '/contact',
     },
   ];
   return (
-    <div className="mt-10">
+    <div className='mt-10'>
       <nav
-        className="flex justify-between items-center bg-white text-black px-10 py-3 rounded-md xl:relative z-10"
-        x-data="{navbarOpen:false}"
+        className='flex justify-between items-center bg-white text-black px-10 py-3 rounded-md xl:relative z-10'
+        x-data='{navbarOpen:false}'
       >
-        <Image src={logo} alt="logo" className="w-[20%] md:w-[10%]" />
-        <ul className="hidden md:flex gap-6 ">
+        <Image src={logo} alt='logo' className='w-[20%] md:w-[10%]' />
+        <ul className='hidden md:flex gap-6 '>
           {li.map((li) => (
             <li
               key={li.id}
-              className="text-[15px] font-bold tracking-widest hover:bg-blue-500 hover:text-white  px-3 py-2 "
+              className='text-[15px] font-bold tracking-widest hover:bg-blue-500 hover:text-white  px-3 py-2 '
             >
-              <motion.div
-                whileHover={{ scale: 1.1 }}
-                transition={{ delay: 0.2 }}
-              >
+              <motion.div whileHover={{ scale: 1.1 }} transition={{ delay: 0.2 }}>
                 <Link href={li.link}>{li.list}</Link>
               </motion.div>
             </li>
           ))}
         </ul>
-        <div className="relative">
+        <div className='relative'>
           <button
             onClick={toggleMenu}
-            className="flex items-center p-2 text-gray-700 bg-gray-100 rounded-md focus:outline-none "
+            className='flex items-center p-2 text-gray-700 bg-gray-100 rounded-md focus:outline-none '
           >
             {isOpen ? (
-              <MdOutlineRestaurantMenu className="w-6 h-6" color="black" />
+              <IoCloseOutline className='w-6 h-6' color='black' />
             ) : (
-              <CiMenuFries className="w-6 h-6" />
+              <CiMenuFries className='w-6 h-6' />
             )}
           </button>
 
           <div
             className={`absolute right-0 w-48 mt-2 py-2 bg-white border rounded-md shadow-xl ${
-              isOpen ? "block" : "hidden"
+              isOpen ? 'block' : 'hidden'
             }`}
           >
             <ul>
               {li.map((li) => (
                 <li
                   key={li.id}
-                  className="text-[15px] font-bold tracking-widest hover:bg-blue-500 hover:text-white  px-3 py-2 "
+                  className='text-[15px] font-bold tracking-widest hover:bg-blue-500 hover:text-white  px-3 py-2 '
                 >
-                  <motion.div
-                    whileHover={{ scale: 1.1 }}
-                    transition={{ delay: 0.2 }}
-                  >
+                  <motion.div whileHover={{ scale: 1.1 }} transition={{ delay: 0.2 }}>
                     <Link href={li.link}>{li.list}</Link>
                   </motion.div>
                 </li>
